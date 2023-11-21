00e411bff50c9784bb4f479b7c021ab994d98b16
Nandha125 <nandhanaddy.125@gmail.com>
2023-11-21 22:51:50 +0530

    merge interval : referred median 2 arr: referred

--- Assignments/FindMedianOf2Array.java
+++ Assignments/FindMedianOf2Array.java
@@ -58,46 +58,39 @@ public class FindMedianOf2Array {
 	
 	// Approach 2: 
 	static double findMedian2(int[] nums1, int[] nums2) {
-		
-		int m = nums1.length;
-		int n = nums2.length;
-		
-		boolean flag = false;
-		double median = 0.0;
-		int mid1 = (m-1)/2;
-		int mid2 = (n-1)/2;
-		int l1 = mid1;
-		int r1 = mid1+1;
-		int l2 = mid2;
-		int r2 = mid2+1;
-		
-		while(!flag) {
-			int min1 = (l1<0)?Integer.MIN_VALUE:nums1[l1]; 
-			int max1 = (r1>m-1)?Integer.MAX_VALUE:nums1[r1];
-			int min2 = (l2<0)?Integer.MIN_VALUE:nums2[l2]; 
-			int max2 = (r2>n-1)?Integer.MAX_VALUE:nums2[r2];
+		int n1 = nums1.length;
+		int n2 = nums2.length;
+		if(n2< n1){
+			return findMedian2(nums2, nums1);
+		}
+		boolean even = false;
+		if((n1+n2)%2 == 0){
+			even = true;
+		}
 
-			if(min1 <= max2 && min2 <= max1) {
-				if((m+n)%2==0) {
-					median = (double)(Math.max(min1,min2)+Math.min(max1,max2))/2;
-				}else {
-					median = Math.max(min1, min2);
+		int low = 0;
+		int high = n1;
+		double median = 0.0;
+		while(low <= high){
+			int mid1 = (low+high)/2; //0
+			int mid2 = (n1+n2+1)/2- mid1;
+			int l1 = (mid1 == 0 ? Integer.MIN_VALUE : nums1[mid1-1]);
+			int l2 = (mid2 == 0 ? Integer.MIN_VALUE : nums2[mid2-1]);
+			int r1 = (mid1 == n1 ? Integer.MAX_VALUE : nums1[mid1]);
+			int r2 = (mid2 == n2 ? Integer.MAX_VALUE : nums2[mid2]);
+//			System.out.println(l1+ " " + l2 + " " + r1 + " " + r2);
+			if(l1 <= r2 && l2 <= r1){
+				if(!even){
+					median = Math.max(l1,l2);
+				}else{
+					median = (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
 				}
-				flag = true;
-			}else {
-				if(min1 > max2) {
-					l1--;
-					r1--;
-					l2++;
-					r2++;
-				}
-				if(min2 > max2) {
-					l1++;
-					r1++;
-					l2--;
-					r2--;
-				}
-				flag = false;
+				break;
+			}
+			if(l1 > r2){
+				high = mid1-1;
+			}else{
+				low = mid1+1;
 			}
 		}
 		return median;
